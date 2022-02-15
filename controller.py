029d635126025c38493aba28af31b17fd9979eb8
unknown <kovgankosvetlana.com>
2022-02-15 14:16:48 +0300

    Calc1

--- controller.py
+++ controller.py
@@ -2,12 +2,14 @@
 import complex_calc
 import float_calc
 import view
+import logic
 
 def complex():
     number1 = view.number_complex()
     number2 = view.number_complex()
     operation = view.opern()
     result = complex_calc.calc(number1, number2, operation)
+    logic.log_in_file(number1, number2, operation, result)
     view.out_data(result)
 
 def float():
@@ -15,4 +17,5 @@ def float():
     number2 = view.number_float()
     operation = view.opern()
     result = float_calc.calc(number1, number2, operation)
+    logic.log_in_file(number1, number2, operation, result)
     view.out_data(result)
